7cece50bab55f7fd038595af23b1290444587c39
QHY2005 <hqin@smith.edu>
2026-02-10 21:03:54 -0500

    2.10

--- computer.py
+++ computer.py
@@ -1,10 +1,31 @@
 class Computer:
+    """
+    This is the computer store that contains the information about a single computer in the resale shop.
+    """
 
-    # What attributes will it need?
+    def __init__(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, price: int):
+        """Initialize and define the computer information."""
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def Update_Price(self, new_price: int) -> None:
+        self.price = new_price
 
-    # What methods will you need?
+    def Update_OS(self, new_os: str) -> None:
+        self.operating_system = new_os
+
+    def __str__(self):
+        description = self.description
+        processor = " Processor: " + self.processor_type
+        storage = " Storage: " + str(self.hard_drive_capacity)
+        memory = " Memory: " + str(self.memory)
+        os = " OS: " + self.operating_system
+        year = " Year: " + str(self.year_made)
+        price = " Price: " + str(self.price)
+        return description + processor + storage + memory + os + year + price
+
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,39 @@
 class ResaleShop:
+    """Tells people this is a resale shop."""
+    
+    def __init__(self):
+        """Initialize the resale shop with an empty inventory."""
+        self.inventory = []  
 
-    # What attributes will it need?
+    def buy(self, computer):
+        """After buy in a computer, this add a computer to the inventory."""
+        self.inventory.append(computer)
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def sell(self, computer):
+        """After sell a computer, this remove a computer from the inventory."""
+        if computer in self.inventory:
+            self.inventory.remove(computer)
+        else:
+            print("Sorry, computer not found in inventory.")
 
-    # What methods will you need?
+
+    def update_price(self, computer, new_price):
+        """If there's a change in price of a certain computer, this enables us to update the price of a computer in the inventory."""         # ⭐
+        if computer in self.inventory:
+            computer.update_price(new_price)
+        else:
+            print("Error: computer not found in inventory.")  
+
+    def print_inventory(self):
+        """ This prints all computers in inventory with quantity."""
+        if not self.inventory:
+            print("Inventory empty.")
+            return
+        printed = []
+
+        for c in self.inventory:
+            if c not in printed:
+                qty = self.inventory.count(c)
+                print(c, "Quantity:", qty)
+                printed.append(c)
+
